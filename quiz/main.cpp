53d0b0c25afed9d0ec58a3bb421af94ea1c735a9
Jasiph <jasiph@gmail.com>
2020-09-17 09:01:52 +0700

    init

---
+++ quiz/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Member {
+    string nama;
+
+    void cetakNama() {
+        cout << "Nama : " << nama << endl;
+    }
+}
+
+int main()
+{
+    Member m;
+    return 0;
+}
